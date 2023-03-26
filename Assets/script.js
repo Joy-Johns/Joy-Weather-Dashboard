a7c5a58b6689d9d5635a1969b76ea55336170c11
Joy Johns <JRJ20716@gmail.com>
2023-03-26 19:05:56 -0400

    Added CSS, update search button look & activated

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,10 @@
 let apiKey = "23d542f35a564bd8824f6ff80d210e45";
 let currentCity = "";
 let lastCity = ""; 
+
+function test(){
+    console.log("TaDa");
+    currentCity = document.getElementById("search-city").value;
+    console.log("Current City!" + currentCity)
+}
+document.getElementById("search-button").addEventListener("click", test);
